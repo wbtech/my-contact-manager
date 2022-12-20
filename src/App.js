ce12bb947d39ced1676fdf5404d6b951e30b030e
Wale Osho <90667947+wbtech@users.noreply.github.com>
2022-12-20 01:24:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import AddContact from "./Component/AddContact";
+import ContactList from "./Component/ContactList";
+import Header from "./Component/Header";
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const contactHandle = (contact) => {
+    console.log(contact);
+    setContacts(...contacts, contact);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <AddContact contactHandle={contactHandle} />
+      <ContactList contacts={contacts} />
+    </>
   );
-}
+};
 
 export default App;
