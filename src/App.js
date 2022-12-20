a49ff7402b258eb22826f8d8a421903086270b5f
Wale Osho <90667947+wbtech@users.noreply.github.com>
2022-12-20 23:33:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./index.css";
 import AddContact from "./Component/AddContact";
 import ContactList from "./Component/ContactList";
@@ -6,17 +8,53 @@ import Header from "./Component/Header";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
+  const LOCALSTORAGE = "contacts";
 
-  const contactHandle = (contact) => {
+  const contactHandler = (contact) => {
     console.log(contact);
-    setContacts(...contacts, contact);
+    setContacts([...contacts, { id: Math.random().toString(), ...contact }]);
   };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
+  };
+
+  //set and get local storage
+  useEffect(() => {
+    const retrieveContact = JSON.parse(localStorage.getItem(LOCALSTORAGE));
+    if (retrieveContact) setContacts(retrieveContact);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCALSTORAGE, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <>
-      <Header />
-      <AddContact contactHandle={contactHandle} />
-      <ContactList contacts={contacts} />
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <ContactList
+                contacts={contacts}
+                getContactId={removeContactHandler}
+              />
+            }
+          />
+          <Route
+            path='/add'
+            element={<AddContact contactHandler={contactHandler} />}
+          />
+        </Routes>
+
+        {/* <AddContact contactHandler={contactHandler} />
+        <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+      </Router>
     </>
   );
 };
